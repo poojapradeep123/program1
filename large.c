f3b44b048e634d7ed39391e66b1fbeaf1123e92b
pooja <poojakpradeep002@gmail.com>
2022-11-15 10:19:37 +0530

    first program

---
+++ large.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+ int a,b;
+ printf("\n enter two numbers");
+ scanf("%d%d",&a,&b);
+ if(a>b)
+ printf("\n %d a is greater",a);
+ else
+ printf("\n %d b is greater",b);
+}
